f2f28b56dc095907726b41930d3574a2356b72bd
YAEL KFIR <yael.kfir6@gmail.com>
2017-10-09 07:58:28 +0300

    master-of-inputs 3

--- src/app/master-of-two-inputs/full-name-inputs/two-inputs.component.ts
+++ src/app/master-of-two-inputs/full-name-inputs/two-inputs.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import 'rxjs/Rx';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-full-name-inputs',
@@ -16,13 +17,34 @@ export class FullNameInputsComponent implements OnInit {
   private inputOne$ = new BehaviorSubject({q: ''});
   private inputTwo$ = new BehaviorSubject({q: ''});
 
+  manyInputsArr = ['first name', 'family name', 'input c', 'input d', 'input e', 'input f', 'input g'];
+  manyInputsObj = {};
+  manyInputsValue = '';
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
+    this.buildManyInputs();
+    this.merageInputs();
+  }
+
+  buildManyInputs() {
+    this.manyInputsArr.forEach(str => this.manyInputsObj[str] = '');
+  }
+
+  handelInputs(event, input: string) {
+    if (input === 'one') {
+      this.inputOne$.next(event);
+    } else if (input === 'two') {
+      this.inputTwo$.next(event);
+    } else {
+      this.freeStyleInputs(event['q'], input)
+    }
+  }
+
+  merageInputs() {
     this.inputOne$.mergeMap((value) => {
-      return this.inputTwo$.map( (value2) => {
+      return this.inputTwo$.map((value2) => {
         return `${value['q']} ${value2['q']}`
       })
     }).subscribe((data) => {
@@ -30,13 +52,17 @@ export class FullNameInputsComponent implements OnInit {
     })
   }
 
-  handelInputs(event, input: string) {
-    console.log(event, 'yay!');
-    if (input === 'one') {
-      this.inputOne$.next(event);
-    } else {
-      this.inputTwo$.next(event);
+  freeStyleInputs(event, i: string) {
+    this.manyInputsObj[i] = event;
+    let value = '';
+    for (const k in this.manyInputsObj) {
+      if (this.manyInputsObj.hasOwnProperty(k)) {
+        value = `${value} ${this.manyInputsObj[k]}`
+      }
     }
+    this.manyInputsValue = value;
   }
-
 }
+
+
+
